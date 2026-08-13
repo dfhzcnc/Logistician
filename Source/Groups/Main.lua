fc53b3a50486a1697971f43e523fd6c96b63bdea
SleepWalker9 <dfhzcnc@users.noreply.github.com>
2026-08-13 16:41:44 -0500

    Initialize empty selling group storage

--- Source/Groups/Main.lua
+++ Source/Groups/Main.lua
@@ -3,34 +3,34 @@ Logistician.Groups.CallbackRegistry:OnLoad()
 Logistician.Groups.CallbackRegistry:GenerateCallbackEvents(Logistician.Groups.Constants.Events)
 
 function Logistician.Groups.Initialize()
-  if LOGISTICIAN_SELLING_GROUPS == nil then
-    LOGISTICIAN_SELLING_GROUPS = {
-      Version = 1,
-      CustomGroups = {},
-      HiddenItems = {},
-    }
+  LOGISTICIAN_SELLING_GROUPS = LOGISTICIAN_SELLING_GROUPS or {}
+  LOGISTICIAN_SELLING_GROUPS.Version = LOGISTICIAN_SELLING_GROUPS.Version or 1
+  LOGISTICIAN_SELLING_GROUPS.CustomGroups = LOGISTICIAN_SELLING_GROUPS.CustomGroups or {}
+  LOGISTICIAN_SELLING_GROUPS.HiddenItems = LOGISTICIAN_SELLING_GROUPS.HiddenItems or {}
 
+  if LOGISTICIAN_SELLING_GROUPS.CustomSections then
+    LOGISTICIAN_SELLING_GROUPS.CustomGroups = LOGISTICIAN_SELLING_GROUPS.CustomSections
+    LOGISTICIAN_SELLING_GROUPS.CustomSections = nil
+  end
+
+  local favouritesIndex = Logistician.Groups.GetGroupIndex("FAVOURITES")
+    or Logistician.Groups.GetGroupIndex("FAVOURITES_GROUP")
+  if favouritesIndex == nil then
     Logistician.Groups.AddGroup("FAVOURITES")
     local list = Logistician.Groups.GetGroupList("FAVOURITES")
 
     for _, data in pairs(Logistician.Config.Get(Logistician.Config.Options.SELLING_FAVOURITE_KEYS)) do
       table.insert(list, data.itemLink)
     end
-  end
-  if LOGISTICIAN_SELLING_GROUPS.CustomSections then
-    LOGISTICIAN_SELLING_GROUPS.CustomGroups = LOGISTICIAN_SELLING_GROUPS.CustomSections
-    LOGISTICIAN_SELLING_GROUPS.CustomSections = nil
+    favouritesIndex = #LOGISTICIAN_SELLING_GROUPS.CustomGroups
   end
 
   local group1 = LOGISTICIAN_SELLING_GROUPS.CustomGroups[1]
-  if group1.name ~= "FAVOURITES_GROUP" and group1.name ~= "FAVOURITES" then
-    for index, group in ipairs(LOGISTICIAN_SELLING_GROUPS.CustomGroups) do
-      if group.name == "FAVOURITES_GROUP" or group.name == "FAVOURITES" then
-        LOGISTICIAN_SELLING_GROUPS.CustomGroups[index] = group1
-        LOGISTICIAN_SELLING_GROUPS.CustomGroups[1] = group
-        group1 = group
-      end
-    end
+  if favouritesIndex ~= 1 then
+    local favourites = LOGISTICIAN_SELLING_GROUPS.CustomGroups[favouritesIndex]
+    LOGISTICIAN_SELLING_GROUPS.CustomGroups[favouritesIndex] = group1
+    LOGISTICIAN_SELLING_GROUPS.CustomGroups[1] = favourites
+    group1 = favourites
   end
 
   if group1.name == "FAVOURITES_GROUP" then
